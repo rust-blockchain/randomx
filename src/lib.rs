c3003b2d223cc2a07cd004976ea068f8dc94282b
Wei Tang <hi@that.world>
2019-09-23 10:05:35 +0800

    Add support for full VM

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,73 @@
 pub const HASH_SIZE: usize = sys::RANDOMX_HASH_SIZE as usize;
 
+pub struct FullVM {
+	cache_ptr: *mut sys::randomx_cache,
+	dataset_ptr: *mut sys::randomx_dataset,
+	ptr: *mut sys::randomx_vm,
+}
+
+impl FullVM {
+	pub fn new(key: &[u8]) -> Self {
+        let cache_ptr = unsafe {
+            let ptr = sys::randomx_alloc_cache(
+				sys::randomx_flags_RANDOMX_FLAG_JIT
+			);
+            sys::randomx_init_cache(
+                ptr,
+                key.as_ptr() as *const std::ffi::c_void,
+                key.len()
+            );
+
+            ptr
+        };
+
+		let dataset_ptr = unsafe {
+			let ptr = sys::randomx_alloc_dataset(
+				sys::randomx_flags_RANDOMX_FLAG_DEFAULT
+			);
+			let count = sys::randomx_dataset_item_count();
+			sys::randomx_init_dataset(ptr, cache_ptr, 0, count);
+			ptr
+		};
+
+		let ptr = unsafe {
+			sys::randomx_create_vm(
+				sys::randomx_flags_RANDOMX_FLAG_JIT |
+				sys::randomx_flags_RANDOMX_FLAG_FULL_MEM,
+				cache_ptr,
+				dataset_ptr
+			)
+		};
+
+		Self { cache_ptr, dataset_ptr, ptr }
+	}
+
+	pub fn calculate(&mut self, input: &[u8]) -> [u8; HASH_SIZE] {
+        let ret = [0u8; HASH_SIZE];
+
+        unsafe {
+            sys::randomx_calculate_hash(
+                self.ptr,
+                input.as_ptr() as *const std::ffi::c_void,
+                input.len(),
+                ret.as_ptr() as *mut std::ffi::c_void,
+            );
+        }
+
+        ret
+    }
+}
+
+impl Drop for FullVM {
+    fn drop(&mut self) {
+        unsafe {
+            sys::randomx_release_cache(self.cache_ptr);
+			sys::randomx_release_dataset(self.dataset_ptr);
+            sys::randomx_destroy_vm(self.ptr);
+        }
+    }
+}
+
 pub struct VM {
     cache_ptr: *mut sys::randomx_cache,
     ptr: *mut sys::randomx_vm,
@@ -8,7 +76,9 @@ pub struct VM {
 impl VM {
     pub fn new(key: &[u8]) -> Self {
         let cache_ptr = unsafe {
-            let ptr = sys::randomx_alloc_cache(sys::randomx_flags_RANDOMX_FLAG_DEFAULT);
+            let ptr = sys::randomx_alloc_cache(
+				sys::randomx_flags_RANDOMX_FLAG_JIT
+			);
             sys::randomx_init_cache(
                 ptr,
                 key.as_ptr() as *const std::ffi::c_void,
@@ -20,7 +90,7 @@ impl VM {
 
         let ptr = unsafe {
             sys::randomx_create_vm(
-                sys::randomx_flags_RANDOMX_FLAG_DEFAULT,
+                sys::randomx_flags_RANDOMX_FLAG_JIT,
                 cache_ptr,
                 std::ptr::null_mut()
             )
@@ -64,4 +134,13 @@ mod tests {
         let hash = vm.calculate(&b"RandomX example input"[..]);
         assert_eq!(hash, [210, 164, 216, 149, 3, 68, 116, 1, 239, 110, 111, 48, 180, 102, 53, 180, 91, 84, 242, 90, 101, 12, 71, 70, 75, 83, 17, 249, 214, 253, 71, 89]);
     }
+
+	#[test]
+	fn should_work_with_full_vm() {
+		let mut vm = VM::new(&b"RandomX example key"[..]);
+        let hash = vm.calculate(&b"RandomX example input"[..]);
+		let mut full_vm = FullVM::new(&b"RandomX example key"[..]);
+        let full_hash = full_vm.calculate(&b"RandomX example input"[..]);
+		assert_eq!(hash, full_hash);
+	}
 }
